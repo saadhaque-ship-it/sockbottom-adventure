2238461ab573ca38fd26c35c66f1509c7ba04179
Sudip-Ahmad <25-63617-3@student.aiub.edu>
2026-04-18 07:52:37 +0600

    world building further done

--- sockbottom/sockbottom.gd
+++ sockbottom/sockbottom.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 130
-const JUMP_VELOCITY = -400
+const JUMP_VELOCITY = -500
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -14,6 +14,11 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	# Attack (E)
+	if Input.is_key_pressed(KEY_E):
+		animated_sprite.play("attack")
+		return
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
